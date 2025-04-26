d750ec1dbb5950f19835f74b0abfb7615b40fe38
Nahom Tsegaye <tnahom214@gmail.com>
2025-04-26 22:42:59 +0300

    url fixed

--- frontend/src/components/Advice.jsx
+++ frontend/src/components/Advice.jsx
@@ -99,7 +99,8 @@ const Advice = () => {
       );
 
       // Send to AI endpoint
-      const response = await fetch('http://localhost:4000/api/generate', {
+        // REMEMBER TO CHANGE IT TO LOCALHOST 4000 HWEN TESTING
+      const response = await fetch('https://ai-expense-tracker-4.onrender.com/api/generate', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({
--- frontend/src/components/Chatbot.jsx
+++ frontend/src/components/Chatbot.jsx
@@ -36,14 +36,14 @@ const Chatbot = () => {
     setInput("");
     setIsTyping(true);
   
+    // REMEMBER TO CHANGE IT TO LOCALHOST 4000 HWEN TESTING
     try {
-      const res = await fetch("http://localhost:4000/api/chat", {
+        const res = await fetch("https://ai-expense-tracker-4.onrender.com/api/chat", {
         method: "POST",
         credentials: "include",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ message: content }),
       });
-  
       const responseData = await res.json();
       
       if (!res.ok) {
